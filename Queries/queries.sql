e27849f17f16608d3fc6129d514756d072798316
Muzznah Ansari <muzznahansari@gmail.com>
2020-05-30 19:07:40 -0400

    retirement table

---
+++ Queries/queries.sql
@@ -0,0 +1,52 @@
+--Get employees that are retiring soon(Born btw 1952-1955).
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+--Get employees that were born in 1952 and will retire soon.
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+--Retirement eligibility.
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+
+SELECT first_name, last_name
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+
+-- Retirement eligibility with hire date variable.
+SELECT first_name, last_name
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Number of employees retiring.
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--CReating retirement_info table.
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+--Check the new table made.
+SELECT * FROM retirement_info;
--- SQL/schema.sql
+++ SQL/schema.sql
@@ -37,12 +37,12 @@ CREATE TABLE salaries (
 
 CREATE TABLE dept_emp (
   	emp_no INT NOT NULL,
-  	dept_no VARCHAR NOT NULL,
+  	dept_no VARCHAR(4) NOT NULL,
   	from_date DATE NOT NULL,
   	to_date DATE NOT NULL,
   	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-  	PRIMARY KEY (emp_no)
+  	PRIMARY KEY (emp_no, dept_no)
 );
 
 CREATE TABLE titles (
@@ -51,7 +51,28 @@ CREATE TABLE titles (
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	PRIMARY KEY (emp_no)
+	PRIMARY KEY (emp_no, title, from_date, to_date)
 );
 
 SELECT * FROM departments;
+
+DROP TABLE dept_emp;
+
+DROP TABLE titles;
+
+SELECT * FROM dept_manager;
+
+SELECT * FROM dept_emp;
+
+SELECT * FROM employees;
+
+SELECT * FROM salaries;
+
+SELECT * FROM titles;
+
+--CReating retirement_info table.
+SELECT first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
